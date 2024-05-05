4853068f44088fa89ffd6ad0e1c703ee30421e08
Кирилл Тепляков <kirillteplycov888@gmail.com>
2024-05-05 18:33:43 +0500

    Реализованный контроллер. Тепляков

---
+++ Controllers/TaskController.cs
@@ -0,0 +1,58 @@
+﻿using API_Тепляков.Context;
+using API_Тепляков.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace API_Тепляков.Controllers
+{
+    [Route("api/TaskController")]
+    public class TaskController : Controller
+    {
+        /// <summary>
+        /// Получение списка задач
+        /// </summary>
+        /// <remarks>Данный метод получает список задач, находящийся в базе данных</remarks>
+        /// <response code="200">Список успешно получен</response>
+        /// <response code="500">При выполнении запроса возникли ошибки</response>
+        [Route("List")]
+        [HttpGet]
+        [ProducesResponseType(typeof(List<Task>), 200)]
+        [ProducesResponseType(500)]
+        public ActionResult List()
+        {
+            try
+            {
+                IEnumerable<Task> Tasks = new TaskContext().Tasks;
+                return Json(Tasks);
+            }
+            catch(Exception e)
+            {
+                return StatusCode(500);
+            }
+        }
+        /// <summary>
+        /// Получение задачи
+        /// </summary>
+        /// <remarks>Данный метод получает задачу, находящуюся в базе данных</remarks>
+        /// <response code="200">Задача успешно получена</response>
+        /// <response code="500">При выполнении запроса возникли ошибки</response>
+        [Route("Item")]
+        [HttpGet]
+        [ProducesResponseType(typeof(Task), 200)]
+        [ProducesResponseType(500)]
+        public ActionResult Item(int id)
+        {
+            try
+            {
+                Task Task = new TaskContext().Tasks.First(x => x.Id == id);
+                return Json(Task);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500);
+            }
+        }
+    }
+}
