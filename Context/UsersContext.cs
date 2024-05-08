ad5da3bd7a004004107b459a09908103a336c99a
Кирилл Тепляков <kirillteplycov888@gmail.com>
2024-05-08 23:37:40 +0500

    Создание модели и контекста пользователя. Тепляков

---
+++ Context/UsersContext.cs
@@ -0,0 +1,26 @@
+﻿using API_Тепляков.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace API_Тепляков.Context
+{
+    public class UsersContext : DbContext
+    {
+        /// <summary>
+        /// Данные из базы данных
+        /// </summary>
+        public DbSet<Users> Users { get;set; }
+        /// <summary>
+        /// Конструктор контекста
+        /// </summary>
+        public UsersContext()
+        {
+            Database.EnsureCreated();
+            Users.Load();
+        }
+        /// <summary>
+        /// Переопределяем метод конфигурации
+        /// </summary>
+        /// <param name="optionsBuilder"></param>
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseMySql("server=localhost;uid=root;pwd=root;database=TaskManager", new MySqlServerVersion(new System.Version(8, 0, 11)));
+    }
+}
